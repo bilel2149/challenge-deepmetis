fb147e9cd444a702e7d1338162896fe8ae775930
Bilel Dhaou <bileldhaou2149@gmail.com>
2022-12-26 21:12:57 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <PokimonsList :title="title" :description="description" :pokemons="pokemons" :is_main_series="is_main_series" v-if="!loading"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+import PokimonsList from './components/PokimonsList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PokimonsList
+  },
+  data(){
+    return{
+      pokemons: [],
+      title: '',
+      description: '',
+      is_main_series: false,
+      loading: true,
+    }
+  },
+  mounted() {
+    this.getGame();
+  },
+  methods: {
+    getGame(){
+        this.loading = true;
+        axios.get('https://pokeapi.co/api/v2/pokedex/2').then (data => {
+            this.title = data.data.name;
+            this.description = data.data.descriptions;
+            this.is_main_series = data.data.is_main_series;
+            this.pokemons = data.data.pokemon_entries;
+            this.loading = false;
+        })
+        .catch ((error) => {
+            console.error (error);
+            this.loading = false;
+        });
+    },
   }
 }
 </script>
@@ -19,7 +47,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ src/components/PokimonsList.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="hello">
+        <h1>{{title}}</h1>
+        <span v-if="is_main_series">is main series</span>
+        <p>Description : {{description_en}}</p>
+        <ul>
+            <li v-for="(pok, index) in pokemons" :key="index">
+                {{pok.pokemon_species.name}}
+            </li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+  name: 'HelloGame',
+  props: {
+    title: String,
+    description: Array,
+    is_main_series: Boolean,
+    pokemons: Array,
+  },
+  data(){
+    return{
+      description_en: '',
+    }
+  },
+  mounted() {
+    this.description_en = this.description[3].description;
+  },
+}
+</script>
+
+<style scoped>
+</style>
